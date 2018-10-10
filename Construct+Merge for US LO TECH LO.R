198c50f1b1267d87bbb5439f127631016c9bf73d
OUYANG2004 <43834616+OUYANG2004@users.noreply.github.com>
2018-10-10 10:15:26 +0800

    Update and rename Construct MERGE for US LO TECH LO.R to Construct+Merge for US LO TECH LO.R

--- Construct+Merge for US LO TECH LO.R
+++ Construct+Merge for US LO TECH LO.R
@@ -1,18 +1,10 @@
-
-
-
-MERGE <- function(dirb,output){
-	
+########with names and out put format#######
+MERGE <- function(dirb,output){	
 	nop = length(dirb[,1])
-	
 	nopt=seq(1:nop)
-	
 	summary = as.data.frame(matrix(0,nrow=22))
-	
 	BM="Benchmark"
-	
 	rownames(summary)=c("Pn","DATE","Inital NAV","Current NAV","NAV closing on precious LOM","Daily P/L", "Daily % change","Long NAV","Short NAV", "Long % daily P/L change","Short % daily P/L change","Long exposure %","Short exposure %","Net exposure %", "MTD % change" ,"Change since inception",paste(BM,"Inital"), paste(BM,"Today"), paste(BM,"closing on precious LOM"),paste(BM,"daily % change"),paste(BM,"MTD % change"),paste(BM,"Change since inception"))
-	
 	for(i in 1:nop){
 		setwd(dirb[i,1])
 		summarypath=paste0(dirb[i,1],"/summary_",dirb[i,2],".csv")
@@ -27,35 +19,20 @@ MERGE <- function(dirb,output){
 			NAME="TECH LO 2"
 		summary1 = rbind(NAME,summary1)
 		summary = cbind(summary,summary1[,-1])
-	}
-	
+	}	
 	summary=summary[,-1]
 	summary=summary[,order(summary[2,])]
-
-
 	setwd(output)
 	write.csv(summary,"summarytotal.csv")
-	
 }
 
 
-
-
-
 ##########
-
 nop=4
-
 dirb=data.frame(diretion=rep(0,nop),name=rep(0,nop))
-
 dirb[1,]=c("/Users/ouyangming/Desktop/U8293808","U8293808")
-
 dirb[2,]=c("/Users/ouyangming/Desktop/U8383220","U8383220")
-
 dirb[3,]=c("/Users/ouyangming/Desktop/U9721087","U9721087")
-
 dirb[4,]=c("/Users/ouyangming/Desktop/U9801918","U9801918")
-
 output = "/Users/ouyangming/Desktop"
-
 summaryt = MERGE(dirb,output)
